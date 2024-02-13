4f64753714227393c08d146632a6b3f8da135149
k-vruttika <148611124+k-vruttika@users.noreply.github.com>
2024-02-13 14:05:41 +0530

    Create multiply.c

---
+++ multiply.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    double a, b, product;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);  
+    product = a * c;
+    printf("Product = %.2lf", product);
+    return 0;
+}
